22b56062debcff83ed490d201a117a1be0617c36
Rafael Silva <rafaeloliveira.cs@gmail.com>
2021-05-26 22:20:51 +0000

    sistema: Add arquivos para abrir e fechar arquivos
    Em commits futuros, as funcoes para coletar metricas de uso de CPU
    e Memoria ira trabalhar com arquivos disponibilizados no diretorio
    /proc. Portanto, ensine duas funcoes para lidar com arquivos:
    
      * AbrirArquivo() - Dado um caminho de um arquivo, em string, retorna
        um arquivo aberto (*os.File).
    
      * FecharArquivo() - Dado um arquivo (*os.File), feche utilizando o
        comando metodo *(arquivo).Close(). A funcao e protegida contra
        arquivos nulos, ou seja, ela e segura para chamar arquivos que
        nao foram abertos pelo sistema.

---
+++ sistema/arquivo_test.go
@@ -0,0 +1,26 @@
+package sistema
+
+import (
+	"testing"
+)
+
+func TestAbrirArquivoNaoExiste(t *testing.T) {
+	arquivo, err := AbrirArquivo("/tmp/nao_existente.1234aa")
+	if arquivo != nil && err == nil {
+		t.Errorf("abrirArquivo() com arquivo inexistente deveria retorna erro")
+	}
+}
+
+func TestAbrirArquivoMemoria(t *testing.T) {
+	if _, err := AbrirArquivo("/proc/meminfo"); err != nil {
+		t.Errorf("abrirArquivo() com arquivo inexistente deveria retorna erro")
+	}
+}
+
+func TestFecharArquivoNulo(t *testing.T) {
+	arquivo, err := AbrirArquivo("/tmp/nao_existente.1234aa")
+	if err == nil {
+		t.Errorf("abrirArquivo() com arquivo inexistente ndeveria retornar erro")
+	}
+	FecharArquivo(arquivo)
+}
